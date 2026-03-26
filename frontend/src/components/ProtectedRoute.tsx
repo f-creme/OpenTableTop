ac8547b99ff3fbfff0ef43ca84c2a90644f4bfee
Florentin Creme <contact.fcreme@gmail.com>
2026-03-26 23:55:03 +0100

    feat(auth): update ProtectedRoute component to redirect to the login page if auth token is not valid

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,12 +1,18 @@
 // components/ProtectedRoute.tsx
 import { Navigate, Outlet } from "react-router-dom"
 import { useRole } from "../context/RoleContext"
+import { useAuth } from "../context/AuthContext"
 
 export default function ProtectedRoute() {
   const { role } = useRole()
+  const { token } = useAuth()
+
+  if (!token) {
+    return <Navigate to="/" replace />
+  }
 
   if (!role) {
-    return <Navigate to="/" replace />
+    return <Navigate to="/room" replace />
   }
 
   return <Outlet />
