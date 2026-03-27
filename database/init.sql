5e36999b2d2b267c1bc0b6696cc270fef9623e9d
Florentin Creme <contact.fcreme@gmail.com>
2026-03-27 22:44:55 +0100

    feat(campaigns): create tables to store information about campaigns and their participants

--- database/init.sql
+++ database/init.sql
@@ -7,3 +7,27 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT NOT NULL, 
     role TEXT DEFAULT 'player'
 );
+
+-- Campaigns table
+CREATE TABLE IF NOT EXISTS campaigns (
+    id SERIAL PRIMARY KEY,
+    campaign_title TEXT UNIQUE NOT NULL
+);
+
+-- Players campaigns
+CREATE TABLE IF NOT EXISTS users_campaigns (
+    id SERIAL PRIMARY KEY, 
+    campaign_id INT NOT NULL,
+    user_id INT NOT NULL,
+    role TEXT DEFAULT 'player',
+    CONSTRAINT fk_campaign 
+        FOREIGN KEY(campaign_id) 
+        REFERENCES campaigns(id) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE, 
+    CONSTRAINT fk_user 
+        FOREIGN KEY(user_id) 
+        REFERENCES users(id)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+);
