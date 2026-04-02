de4ab27d0a66982ea262a0f96a650342346732dd
Florentin Creme <contact.fcreme@gmail.com>
2026-04-03 01:23:28 +0200

    refactor(maps): update get_map function to allow finding a map with a file UUID

--- backend/api/maps/routes.py
+++ backend/api/maps/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import FileResponse
+from pathlib import Path
 import os
 
 from core.config import DATA_DIR
@@ -29,19 +30,27 @@ def list_maps(campaign_uuid: str, db = Depends(get_db)):
         raise HTTPException(status_code=500, detail=str(e))
     
     
-@router.get("/{campaign_id}/{map_name}")
-def get_map(campaign_id: int, map_name: str):
+@router.get("/{campaign_uuid}/{file_uuid}")
+def get_map(campaign_uuid: str, file_uuid: str):
     """Return the selected map"""
 
-    is_safe_filename = secure_urls.is_safe_filename(map_name)
+    is_safe_filename = secure_urls.is_safe_filename(file_uuid)
     if not is_safe_filename:
         raise HTTPException(status_code=400, detail="Invalid file name")
     
-    campaign_dir_name = f"campaign_{campaign_id:04d}"
-    campaign_maps_dir = os.path.join(DATA_DIR, campaign_dir_name, "maps")
-    file_path = os.path.join(campaign_maps_dir, map_name)
+    campaign_dir_name = get_campaign_dir(campaign_uuid=campaign_uuid)
+    maps_dir = Path(campaign_dir_name) / "maps" 
 
-    is_safe_path = secure_urls.is_safe_path(DATA_DIR, file_path)
+    matching_files = list(maps_dir.glob(f"{file_uuid}.*"))
+    if not matching_files:
+        raise HTTPException(status_code=404, detail="File not found")
+    
+    if len(matching_files) > 1:
+        raise HTTPException(status_code=500, detail="Multiple files found for UUID")
+    
+    file_path = matching_files[0]
+
+    is_safe_path = secure_urls.is_safe_path(DATA_DIR, str(file_path))
     if not is_safe_path:
         raise HTTPException(status_code=400, detail="Unauthorized access")
        
