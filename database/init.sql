d8f0217b93566475136ce1d5bb729380232a0483
Florentin Creme <contact.fcreme@gmail.com>
2026-04-02 17:20:39 +0200

    refactor(database): refactor init.sql to use uuid instead of sequential id and create a table to identify files with an uuid

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,7 @@
 
 -- Users table for login
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
     username TEXT UNIQUE NOT NULL, 
     password_hash TEXT NOT NULL, 
     role TEXT DEFAULT 'player'
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS users (
 -- Profiles table
 CREATE TABLE IF NOT EXISTS profiles (
     profile_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
+    user_uuid UUID NOT NULL,
     public_name TEXT UNIQUE,
     CONSTRAINT fk_user_id 
-        FOREIGN KEY(user_id) 
-        REFERENCES users(id) 
+        FOREIGN KEY(user_uuid) 
+        REFERENCES users(uuid) 
         ON DELETE CASCADE 
         ON UPDATE CASCADE 
 );
@@ -24,41 +24,55 @@ CREATE INDEX idx_profiles_public_name ON profiles(public_name);
 
 -- Campaigns table
 CREATE TABLE IF NOT EXISTS campaigns (
-    id SERIAL PRIMARY KEY,
+    campaign_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     campaign_title TEXT UNIQUE NOT NULL
 );
 
 -- Players campaigns
 CREATE TABLE IF NOT EXISTS users_campaigns (
     id SERIAL PRIMARY KEY, 
-    campaign_id INT NOT NULL,
-    user_id INT NOT NULL,
+    campaign_uuid UUID NOT NULL,
+    user_uuid UUID NOT NULL,
     role TEXT DEFAULT 'player',
     character_name TEXT, 
     CONSTRAINT fk_campaign 
-        FOREIGN KEY(campaign_id) 
-        REFERENCES campaigns(id) 
+        FOREIGN KEY(campaign_uuid) 
+        REFERENCES campaigns(campaign_uuid) 
         ON DELETE CASCADE 
         ON UPDATE CASCADE, 
     CONSTRAINT fk_user 
-        FOREIGN KEY(user_id) 
-        REFERENCES users(id)
+        FOREIGN KEY(user_uuid) 
+        REFERENCES users(uuid)
         ON DELETE CASCADE 
         ON UPDATE CASCADE
 );
 
 -- Characters
 CREATE TABLE IF NOT EXISTS characters (
-    id SERIAL PRIMARY KEY, 
-    user_id INT NOT NULL,
+    character_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_uuid UUID NOT NULL,
     name VARCHAR(30) NOT NULL, 
     class VARCHAR(30), 
     appearance VARCHAR(33),
     personality VARCHAR(33),
     bio VARCHAR(144), 
     CONSTRAINT fk_user 
-        FOREIGN KEY(user_id) 
-        REFERENCES users(id)
+        FOREIGN KEY(user_uuid) 
+        REFERENCES users(uuid)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+);
+
+-- File storage
+CREATE TABLE IF NOT EXISTS files (
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    file_name TEXT NOT NULL,
+    file_type TEXT CHECK (file_type IN ('map', 'token', 'illustration', 'portrait')), 
+    owner_uuid UUID NOT NULL, 
+    created_at TIMESTAMP DEFAULT now(), 
+    CONSTRAINT fk_owner 
+        FOREIGN KEY(owner_uuid) 
+        REFERENCES users(uuid) 
         ON DELETE CASCADE 
         ON UPDATE CASCADE
 );
