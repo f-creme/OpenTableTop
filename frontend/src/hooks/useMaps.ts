34eb56ca7f951983c94e8143aebc1cbfb27b964c
Florentin Creme <contact.fcreme@gmail.com>
2026-04-03 01:25:26 +0200

    refactor(maps): update useMaps hook to handle FileType instead of string response

--- frontend/src/hooks/useMaps.ts
+++ frontend/src/hooks/useMaps.ts
@@ -1,34 +1,25 @@
 import { useEffect, useState } from "react";
 import { useCampaign } from "../context/CampaignContext";
 import { getMaps } from "../api/services/mapServices";
+import type { FileType, FileTypeAPI } from "../types/file";
 
 export const useMaps = () => {
     const { campaignId } = useCampaign()
-    const [maps, setMaps] = useState<string[]>([]);
+    const [maps, setMaps] = useState<FileType[]>([]);
     const [selectedMap, setSelectedMap] = useState<string | null>(null);
 
+    const mapFileType = ({uuid, file_name}: FileTypeAPI): FileType => ({uuid: uuid, fileName: file_name}) 
+
     useEffect(() => {
-        if (!campaignId) return;
+        if (!campaignId || campaignId === "__NULL__") return;
         getMaps(campaignId)
-            .then((maps) => {
+            .then((res) => {
+
+                const maps: FileType[] = res.map(mapFileType);
                 setMaps(maps);
-
-                const savedMap = localStorage.getItem("selectedMap");
-
-                if (savedMap && maps.includes(savedMap)) {
-                    setSelectedMap(savedMap);
-                } else if (maps.length > 0) {
-                    setSelectedMap(maps[0]);
-                }
             })
             .catch(console.error);
     }, []);
-
-    useEffect(() => {
-        if (selectedMap) {
-            localStorage.setItem("selectedMap", selectedMap);
-        }
-    }, [selectedMap]);
 
     return {
         maps,
