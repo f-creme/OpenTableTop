23f23bfe0e9e129525236cba506ddd9c4c0f66be
Florentin Creme <contact.fcreme@gmail.com>
2026-04-02 21:34:19 +0200

    refactot: adapt campaignId type in mapServices

--- frontend/src/api/services/mapServices.ts
+++ frontend/src/api/services/mapServices.ts
@@ -1,16 +1,16 @@
 import axiosInstance from "../axiosInstance";
 
-export async function getMaps(campaignId: number) {
+export async function getMaps(campaignId: string) {
     const res = await axiosInstance.get(`/maps/${campaignId}`)
     return res.data.maps
 }
 
-export async function getIllustrations(campaignId: number) {
+export async function getIllustrations(campaignId: string) {
     const res = await axiosInstance.get(`/illus/${campaignId}`);
     return res.data.illus
 }
 
-export async function getTokens(campaignId: number) {
+export async function getTokens(campaignId: string) {
     const res = await axiosInstance.get(`tokens/${campaignId}`);
     return res.data.tokens;
 }
