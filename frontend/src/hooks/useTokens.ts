32ea38fa8b178015bae54570b247dfc041be80e0
Florentin Creme <contact.fcreme@gmail.com>
2026-04-03 02:24:52 +0200

    refactor(tokens): update useTokens hook to support new Token type

--- frontend/src/hooks/useTokens.ts
+++ frontend/src/hooks/useTokens.ts
@@ -1,7 +1,7 @@
 // hooks/useTokens.tsx
 import { useEffect, useState } from "react";
 import { useCampaign } from "../context/CampaignContext";
-import type { Token } from "../types/token";
+import type { Token, TokenAPI } from "../types/token";
 import { getTokens } from "../api/services/mapServices";
 
 export const useTokens = () => {
@@ -9,19 +9,30 @@ export const useTokens = () => {
   const [tokens, setTokens] = useState<Token[]>([]);
   const [activeTokens, setActiveTokens] = useState<Token[]>([]);
 
+  const mapApiToken = ({uuid, file_name, x, y, scale}: TokenAPI): Token => ({
+    uuid: uuid,
+    fileName: file_name,
+    x: x, 
+    y: y, 
+    scale: scale
+  })
+
   useEffect(() => {
     if (!campaignId) return;
 
     getTokens(campaignId)
-      .then((tokens) => setTokens(tokens))
+      .then((res) => {
+        const tokens: Token[] = res.map(mapApiToken);
+        setTokens(tokens)
+      })
       .catch((err) => console.log(err));
   }, []);
 
   const ToggleToken = (b: boolean, token: Token) => {
-    if (b === true && !activeTokens.some((t) => t.id === token.id)) {
+    if (b === true && !activeTokens.some((t) => t.uuid === token.uuid)) {
       setActiveTokens((prev) => [...prev, token]);
-    } else if (b === false && activeTokens.some((t) => t.id === token.id)) {
-      setActiveTokens((prev) => prev.filter((t) => t.id !== token.id));
+    } else if (b === false && activeTokens.some((t) => t.uuid === token.uuid)) {
+      setActiveTokens((prev) => prev.filter((t) => t.uuid !== token.uuid));
     }
   };
 
